71407940849ac18c1147b4478b77e18e14f841d4
NubeDev <aidan@enviroservices.com.au>
2024-01-16 15:33:13 +1100

    added examples for systemctl

--- demo/demo.go
+++ demo/demo.go
@@ -11,20 +11,20 @@ func main() {
 	fmt.Println(err)
 	resp := c.IPValidation("162")
 	fmt.Printf("%+v\n", resp)
-	command, err := c.RunCommand(&rxclient.CommandBody{
-		Command: "pwd",
-		Args:    nil,
-		Timeout: 0,
-	})
+	//command, err := c.RunCommand(&rxclient.CommandBody{
+	//	Command: "date",
+	//	Args:    nil,
+	//	Timeout: 0,
+	//})
 	fmt.Println(err)
-	fmt.Printf("%+v\n", command)
+	//fmt.Printf("%+v\n", command)
 
-	cmdResp, err := c.SystemdCommand("mosquitto", rxclient.SystemCTLStart, 1)
-	fmt.Println(err)
-	fmt.Printf("%+v\n", cmdResp)
-
-	statusResp, err := c.SystemdStatus("mosquitto", 1)
-	fmt.Println(err)
-	fmt.Printf("%+v\n", statusResp)
+	//cmdResp, err := c.SystemdCommand("mosquitto", rxclient.SystemCTLStart, 1)
+	//fmt.Println(err)
+	//fmt.Printf("%+v\n", cmdResp)
+	//
+	//statusResp, err := c.SystemdStatus("mosquitto", 1)
+	//fmt.Println(err)
+	//fmt.Printf("%+v\n", statusResp)
 
 }
